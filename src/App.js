3788611e03499ec137b1e5c624a5a4d813d16635
Mohammad Ali Mashta <m.mashta@tu-braunschweig.de>
2023-11-01 17:47:47 +0100

    gitignore

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          rent your favorite car at your favorite time!
         </p>
         <a
           className="App-link"
